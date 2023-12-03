6da8f16d9c22a2b2c88320c4336521f626e9186a
Fylkirist <112450556+Fylkirist@users.noreply.github.com>
2023-12-03 23:53:15 +0100

    rotation and collision stuff

--- Systems/PhysicsSystem.cs
+++ Systems/PhysicsSystem.cs
@@ -57,7 +57,62 @@ public class PhysicsSystem : SystemBase{
     }
 
     public void HandleCollision2(CollisionEvent2 collisionEvent){
+        var collider1 = collisionEvent.Collider1;
+        var physics1 = World.QueryComponent<Physics2>(collider1);
+        if(physics1 == null){
+            return;
+        }
+
+        var collider2 = collisionEvent.Collider2;
+        var physics2 = World.QueryComponent<Physics2>(collider2);
+        if(physics2 == null){
+            return;
+        }
+
+        var rigidBody1 = World.QueryComponent<RigidBody2>(collider1);
+        var rigidBody2 = World.QueryComponent<RigidBody2>(collider2);
+
+        if(rigidBody1 == null){
+            var softBody1 = World.QueryComponent<SoftBody2>(collider1);
+            if(softBody1 == null){
+                return;
+            }
+            if(rigidBody2 == null){
+                var softBody2 = World.QueryComponent<SoftBody2>(collider2);
+                if(softBody2 == null){
+                    return;
+                }
+                SoftBody2Collision(softBody1, physics1, collisionEvent.CollisionIndex1 ,softBody2, physics2, collisionEvent.CollisionIndex2);
+                return;
+            }   
+            else{
+                RigidSoftBody2Collision(rigidBody2, physics2,collisionEvent.CollisionIndex1, softBody1, physics1,collisionEvent.CollisionIndex1);
+                return;
+            }
+        }
+        if(rigidBody2 == null){
+            var softBody2 = World.QueryComponent<SoftBody2>(collider2);
+            if(softBody2 == null){
+                return;
+            }
+            RigidSoftBody2Collision(rigidBody1, physics1, collisionEvent.CollisionIndex1, softBody2, physics2,collisionEvent.CollisionIndex2);
+            return;
+        }
+
+        RigidBody2Collision(rigidBody1,physics1, collisionEvent.CollisionIndex1, rigidBody2,physics2,collisionEvent.CollisionIndex2);
+
+    }
+
+    private void RigidSoftBody2Collision(RigidBody2 rigidBody, Physics2 rigidPhysics, int rigidIndex, SoftBody2 softBody, Physics2 softPhysics, int softIndex){
         
+    }
+
+    private void SoftBody2Collision(SoftBody2 softBody1, Physics2 physics1, int index1, SoftBody2 softBody2, Physics2 physics2, int index2){
+
+    }
+
+    private void RigidBody2Collision(RigidBody2 rigidBody1, Physics2 physics1, int index1, RigidBody2 rigidBody2, Physics2 physics2, int index2){
+
     }
 
     public void HandleCollision3(CollisionEvent3 collisionEvent){
@@ -126,7 +181,72 @@ public class PhysicsSystem : SystemBase{
        var physicsComponents = World.GetComponents<Physics2>();
        foreach(var component in physicsComponents){
            component.Position += component.Velocity * delta;
+           var rigidBody = World.QueryComponent<RigidBody2>(component.Owner);
+           if(rigidBody != null){
+               UpdateRigidBody2(rigidBody, component, delta);
+           }
        }
+    }
+
+    private void UpdateRigidBody2(RigidBody2 body, Physics2 transform, float delta){
+        var rotationDiff = body.AngularVelocity*delta;
+        for(int i = 0; i<body.Shapes.Length; i++){
+            switch(body.Shapes[i].Type){
+                case ShapeType2D.SymmetricalPolygon:
+                    body.Shapes[i].SymmetricalPolygon.Rotation += rotationDiff;
+                    if(body.Shapes[i].SymmetricalPolygon.Rotation > 2*Math.PI){
+                        body.Shapes[i].SymmetricalPolygon.Rotation -= 2*(float)Math.PI;
+                    }
+                    body.Shapes[i].SymmetricalPolygon.Offset = Vector2.Transform(body.Shapes[i].SymmetricalPolygon.Offset,Matrix3x2.CreateRotation(rotationDiff,transform.Position));
+                    break;
+                    
+                case ShapeType2D.Circle:
+                    body.Shapes[i].Circle.Offset = Vector2.Transform(body.Shapes[i].Circle.Offset,Matrix3x2.CreateRotation(rotationDiff,transform.Position));
+                    break;
+
+                case ShapeType2D.Polygon2:
+                    for(int vIdx = 0; vIdx<body.Shapes[i].Polygon2.NumVertices; vIdx++){
+                        body.Shapes[i].Polygon2.Vertices[vIdx] =
+                            Vector2.Transform(body.Shapes[i].Polygon2.Vertices[vIdx],Matrix3x2.CreateRotation(rotationDiff));
+                        body.Shapes[i].Polygon2.Vertices[vIdx] = 
+                            Vector2.Transform(body.Shapes[i].Polygon2.Vertices[vIdx]+body.Shapes[i].Polygon2.Offset,Matrix3x2.CreateRotation(rotationDiff));
+                    }
+                    body.Shapes[i].Polygon2.Offset = Vector2.Transform(body.Shapes[i].Polygon2.Offset, Matrix3x2.CreateRotation(rotationDiff));
+                    break;
+
+                case ShapeType2D.Triangle:
+                    body.Shapes[i].Triangle.P1 = Vector2.Transform(body.Shapes[i].Triangle.P1, Matrix3x2.CreateRotation(rotationDiff));
+                    body.Shapes[i].Triangle.P1 = Vector2.Transform(body.Shapes[i].Triangle.P1+body.Shapes[i].Triangle.Offset, Matrix3x2.CreateRotation(rotationDiff));
+
+                    body.Shapes[i].Triangle.P2 = Vector2.Transform(body.Shapes[i].Triangle.P2, Matrix3x2.CreateRotation(rotationDiff));
+                    body.Shapes[i].Triangle.P2 = Vector2.Transform(body.Shapes[i].Triangle.P2+body.Shapes[i].Triangle.Offset, Matrix3x2.CreateRotation(rotationDiff));
+
+                    body.Shapes[i].Triangle.P3 = Vector2.Transform(body.Shapes[i].Triangle.P3, Matrix3x2.CreateRotation(rotationDiff));
+                    body.Shapes[i].Triangle.P3 = Vector2.Transform(body.Shapes[i].Triangle.P3+body.Shapes[i].Triangle.Offset, Matrix3x2.CreateRotation(rotationDiff));
+
+                    body.Shapes[i].Triangle.Offset = Vector2.Transform(body.Shapes[i].Triangle.Offset,Matrix3x2.CreateRotation(rotationDiff));
+                    break;
+
+                case ShapeType2D.Rectangle:
+                    body.Shapes[i].Rectangle.P1 = Vector2.Transform(body.Shapes[i].Rectangle.P1, Matrix3x2.CreateRotation(rotationDiff));
+                    body.Shapes[i].Rectangle.P1 = Vector2.Transform(body.Shapes[i].Rectangle.P1 + body.Shapes[i].Rectangle.Offset, Matrix3x2.CreateRotation(rotationDiff));
+
+                    body.Shapes[i].Rectangle.P2 = Vector2.Transform(body.Shapes[i].Rectangle.P2, Matrix3x2.CreateRotation(rotationDiff));
+                    body.Shapes[i].Rectangle.P2 = Vector2.Transform(body.Shapes[i].Rectangle.P2 + body.Shapes[i].Rectangle.Offset, Matrix3x2.CreateRotation(rotationDiff));
+
+                    body.Shapes[i].Rectangle.P3 = Vector2.Transform(body.Shapes[i].Rectangle.P3, Matrix3x2.CreateRotation(rotationDiff));
+                    body.Shapes[i].Rectangle.P3 = Vector2.Transform(body.Shapes[i].Rectangle.P3 + body.Shapes[i].Rectangle.Offset, Matrix3x2.CreateRotation(rotationDiff));
+
+                    body.Shapes[i].Rectangle.P4 = Vector2.Transform(body.Shapes[i].Rectangle.P4, Matrix3x2.CreateRotation(rotationDiff));
+                    body.Shapes[i].Rectangle.P4 = Vector2.Transform(body.Shapes[i].Rectangle.P4 + body.Shapes[i].Rectangle.Offset, Matrix3x2.CreateRotation(rotationDiff));
+
+                    body.Shapes[i].Rectangle.Offset = Vector2.Transform(body.Shapes[i].Rectangle.Offset,Matrix3x2.CreateRotation(rotationDiff));
+                    break;
+
+                default:
+                    break;
+            }
+        }
     }
 
     public void HandleMovement3D(float delta){
@@ -135,7 +255,6 @@ public class PhysicsSystem : SystemBase{
         {
            component.Position += component.Velocity * delta;
            component.Rotation += component.AngularMomentum * delta;
-
         }
     }
 }
